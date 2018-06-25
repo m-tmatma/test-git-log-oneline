551838c3b3a1d27afc32cdd55ea0343af0767136
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-25 21:40:51 +0900

    変数に入れる

--- show-rev.bat
+++ show-rev.bat
@@ -1,2 +1,8 @@
+@echo off
+@rem http://www.robvanderwoude.com/escapechars.php
 set PATH=%PATH%;C:\Program Files\Git\usr\bin
-git log --oneline | wc -l | sed -e "s/[ \t]*//g"
+for /f "usebackq" %%s in (`git log --oneline ^| wc -l ^| sed -e "s/[ \t]*//g"`) do (
+	set REVISIONS=%%s
+)
+
+echo REVISIONS = %REVISIONS%
