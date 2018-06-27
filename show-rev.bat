66f7e4f0573c75708d5c15688c9a9b28e69eef2c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-27 22:20:15 +0900

    commit URL を取得する処理を実装

--- show-rev.bat
+++ show-rev.bat
@@ -17,3 +17,18 @@ echo APPVEYOR_REPO_NAME                = %APPVEYOR_REPO_NAME%
 echo APPVEYOR_REPO_TAG                 = %APPVEYOR_REPO_TAG%
 echo APPVEYOR_REPO_TAG_NAME            = %APPVEYOR_REPO_TAG_NAME%
 echo APPVEYOR_PULL_REQUEST_HEAD_COMMIT = %APPVEYOR_PULL_REQUEST_HEAD_COMMIT%
+echo APPVEYOR_REPO_COMMIT              = %APPVEYOR_REPO_COMMIT%
+
+if "%APPVEYOR_REPO_PROVIDER%" == "gitHub" (
+	set PREFIX_GITHUB=https://github.com
+	@rem Not Pull Request
+	if "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" (
+		@rem https://github.com/m-tmatma/test-git-log-oneline/commit/32b2f9dcede522242b0805f4a12fa290d1c14018
+		set COMMIT_URL=%PREFIX_GITHUB%/%APPVEYOR_REPO_NAME%/commit/%APPVEYOR_REPO_COMMIT%
+	
+	) else (
+		@rem https://github.com/m-tmatma/test-git-log-oneline/pull/2/commits/4a93c7a41c226d75c9e5dbc6031cabd8730cd4a3
+		set COMMIT_URL=%PREFIX_GITHUB%/%APPVEYOR_REPO_NAME%/pull/commits/%APPVEYOR_PULL_REQUEST_NUMBER%/%APPVEYOR_REPO_COMMIT%
+	)
+)
+echo COMMIT_URL                        = %COMMIT_URL%
