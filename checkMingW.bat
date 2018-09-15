cced3fe004b2eb6523c0bf0f8e039bd821132bd9
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-15 21:28:34 +0900

    mingW のチェック追加

---
+++ checkMingW.bat
@@ -0,0 +1,7 @@
+dir C:\MinGW\*.exe
+dir C:\MinGW\bin\*.exe
+dir C:\MinGW\msys\1.0\*.exe
+dir C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\*.exe
+dir C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1\*.exe
+dir C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\*.exe
+dir C:\mingw-w64\x86_64-7.3.0-posix-seh-rt_v5-rev0\*.exe
